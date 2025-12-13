48bcaf2627873df7df0bceaafd09d6c6e8b153c2
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-12-13 23:11:07 +0600

    js file add and logic build

---
+++ index.js
@@ -0,0 +1,26 @@
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("Ali");
+
+
+const add = (a, b) => {
+  return a + b;
+};
+const sub = (a, b) => {
+  return a - b;
+};
+const div = (a, b) => {
+  return a / b;
+};
+
+let nums = [1,2,3];
+let doubled = nums.map(n => n * 2);
+console.log(doubled);
+
+
+
+console.log(add(10, 5));
+console.log(add(20,15));
+console.log(add(20,15));
